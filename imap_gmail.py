f0fa84813c74b8f7a0df6d59d88f8d2ea74b23a1
Joshua Petitt <joshpetitt@jpmec.com>
2014-01-20 10:16:40 -0600

    Initial commit

---
+++ imap_gmail.py
@@ -0,0 +1,106 @@
+import imaplib, re
+from email.parser import HeaderParser
+
+
+class IMAP_GMAIL(imaplib.IMAP4_SSL):
+
+    def __init__(self):
+        imaplib.IMAP4_SSL.__init__(self, 'imap.gmail.com', port = 993)
+
+
+    def mailbox_names(self, directory = None, pattern = None):
+        re_pattern_str = '^(.+) "(.+)" "(.+)"$'
+        re_pattern = re.compile(re_pattern_str)
+
+        if directory is None:
+          status, folder_list = self.list()
+        else:
+          if pattern is None:
+            status, folder_list = self.list(directory)
+          else:
+            status, folder_list = self.list(directory, pattern)
+
+        result = []
+        for folder in folder_list:
+            flags, delimiter, mailbox_name = re_pattern.match(folder).groups()
+            result.append(mailbox_name)
+
+        return result
+
+
+    def messages_count(self, mailbox):
+      status, response = self.status(mailbox, '(MESSAGES)')
+
+      if 'OK' == status:
+        re_pattern_str = '^"{0}" \(MESSAGES (\d+)\)$'.format(mailbox)
+        re_pattern = re.compile(re_pattern_str)
+        re_groups = re_pattern.match(response[0]).groups()
+
+        return int(re_groups[0])
+      else:
+        return 0
+
+
+    def recent_count(self, mailbox):
+      status, response = self.status(mailbox, '(RECENT)')
+
+      if 'OK' == status:
+        re_pattern_str = '^"{0}" \(RECENT (\d+)\)$'.format(mailbox)
+        re_pattern = re.compile(re_pattern_str)
+        re_groups = re_pattern.match(response[0]).groups()
+
+        return int(re_groups[0])
+
+      else:
+        return 0
+
+
+    def unseen_count(self, mailbox):
+      status, response = self.status(mailbox, '(UNSEEN)')
+
+      if 'OK' == status:
+        re_pattern_str = '^"{0}" \(UNSEEN (\d+)\)$'.format(mailbox)
+        re_pattern = re.compile(re_pattern_str)
+        re_groups = re_pattern.match(response[0]).groups()
+
+        return int(re_groups[0])
+
+      else:
+        return 0
+
+
+    def unread_count(self, mailbox, names):
+      status, response = self.status(mailbox, names)
+
+      print status
+      print response
+
+
+      if 'OK' == status:
+
+        pattern_str = '^"(.+)" \(\w+ (.+)\)$'
+        pattern = re.compile(pattern_str)
+
+        m = pattern.match(response[0])
+
+        result = m.group(2)
+        return result
+
+      else:
+        return 0
+
+
+    def fetch_header(self, mailbox):
+
+      self.select(mailbox)
+      status, fetch_data = self.fetch('1', '(BODY.PEEK[HEADER])')
+      self.close()
+
+      header_parser = HeaderParser()
+      header = header_parser.parsestr(fetch_data[0][1])
+
+      if status == 'OK':
+        return header
+      else:
+        return None
+
---
+++ test_imap_gmail.py
@@ -0,0 +1,80 @@
+import unittest
+import imaplib, re, time
+from email.parser import HeaderParser
+
+import imap_gmail
+
+
+
+
+class IMAP_GMAIL(unittest.TestCase):
+
+    def setUp(self):
+      self.gmail = imap_gmail.IMAP_GMAIL()
+
+      username = # user@gmail.com
+      password = # password
+
+      self.gmail.login(username, password)
+
+
+    def tearDown(self):
+      self.gmail.logout()
+
+
+    def test_mailbox_names(self):
+      result = self.gmail.mailbox_names()
+
+      self.assertEqual(result,
+      [ 'INBOX'
+      , '[Gmail]'
+      , '[Gmail]/All Mail'
+      , '[Gmail]/Drafts'
+      , '[Gmail]/Important'
+      , '[Gmail]/Sent Mail'
+      , '[Gmail]/Spam'
+      , '[Gmail]/Starred'
+      , '[Gmail]/Trash'])
+
+
+      result = self.gmail.mailbox_names('[Gmail]')
+
+      self.assertEqual(result,
+      [ '[Gmail]'
+      , '[Gmail]/All Mail'
+      , '[Gmail]/Drafts'
+      , '[Gmail]/Important'
+      , '[Gmail]/Sent Mail'
+      , '[Gmail]/Spam'
+      , '[Gmail]/Starred'
+      , '[Gmail]/Trash'])
+
+
+    def test_messages_count(self):
+      result = self.gmail.messages_count('INBOX')
+
+      self.assertEqual(result, 1)
+
+
+    def test_recent_count(self):
+      result = self.gmail.recent_count('INBOX')
+
+      self.assertEqual(result, 0)
+
+
+    def test_unseen_count(self):
+      result = self.gmail.unseen_count('INBOX')
+
+      self.assertEqual(result, 0)
+
+
+    def test_fetch_header(self):
+      result = self.gmail.fetch_header('INBOX')
+
+      self.assertTrue(result is not None)
+
+
+
+
+if __name__ == "__main__":
+  unittest.main()
